8d8e5d356e56c94247b58135dee8300719dfa551
vchinsomboon <varalee_c@hotmail.com>
2024-07-10 12:48:40 -0700

    test post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,6 +12,13 @@ def hello():
 def get_data():
     data = {'message': 'Hello from Flask!'}
     return jsonify(data)
+
+@app.route('/receive-data', methods=['POST'])
+def receive_data():
+    data = request.json  # Assuming data is sent as JSON
+    # Process the data
+    print(data)  # Print to see data received in the console
+    return jsonify({'message': 'Data received successfully'}), 200
 
 # WSGI callable function for Waitress or Gunicorn
 def create_wsgi():
